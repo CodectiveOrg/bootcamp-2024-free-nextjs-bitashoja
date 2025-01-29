6cefb02f7edb6f15f69f74a1e48e7b053462cfab
bita <shojabita@.com>
2025-01-29 15:40:50 +0330

    [CREATE] details doctors

--- src/app/doctor/[id]/page.tsx
+++ src/app/doctor/[id]/page.tsx
@@ -4,9 +4,10 @@ import { notFound } from "next/navigation";
 import { DoctorModel } from "@/app/search/models/doctor.model";
 import Image from "next/image";
 import styles from "./doctorPage.module.css";
+import SurveyForm from "@/app/components/survey-form/surveyForm";
+
 import MingcuteTelegramFill from "@/icons/MingcuteTelegramFill";
 import MingcuteWhatsappFill from "@/icons/MingcuteWhatsappFill";
-
 type Props = {
   params: { id: string };
 };
@@ -23,20 +24,26 @@ export default async function Page({ params }: Props): Promise<JSX.Element> {
   return (
     <div className={styles.container}>
       <div className={styles.left}>
-        <Image
-          width={50}
-          height={50}
-          src={`https://cdn.paziresh24.com${doctor.image}`}
-          alt={doctor.name}
-          className={styles.image}
-        />
-        <div className={styles.details}>
-          <h3>{doctor.name}</h3>
-          <p>{doctor.brief}</p>
+        <div className={styles.personals}>
+          <div className={styles.personal}>
+            <Image
+              width={50}
+              height={50}
+              src={`https://cdn.paziresh24.com${doctor.image}`}
+              alt={doctor.name}
+              className={styles.image}
+            />
+            <div>
+              <h3>{doctor.name}</h3>
+              <p>{doctor.brief}</p>
+            </div>
+          </div>
           <div className={styles.rating}>
             <span>⭐ {doctor.averageRating.toFixed(1)}</span>
             <span>({doctor.totalVotes} رای)</span>
           </div>
+        </div>
+        <div className={styles.details}>
           <div>
             پذیرش24 بیش از 3 سال و 2 ماه افتخار میزبانی از صفحه اختصاصی{" "}
             {doctor.name} را داشته است.
@@ -47,28 +54,53 @@ export default async function Page({ params }: Props): Promise<JSX.Element> {
             ارائه شده است.
           </p>
         </div>
+        <div className={styles.comments}>
+          <h4>نظرات در مورد {doctor.name}</h4>
+          <SurveyForm />
+        </div>
       </div>
       <div className={styles.right}>
-        <p>مبلغ ویزیت: 200,000 تومان</p>
-        <div>
-          <MingcuteTelegramFill />
-          <MingcuteWhatsappFill />
+        <div className={styles.visits}>
+          <div className={styles.visit}>
+            <ul>
+              <li>همین الان ویزیت آنلاین شوید</li>
+            </ul>
+            <h4>مبلغ ویزیت: 200,000 تومان</h4>
+          </div>
+          <hr />
+          <div>
+            <div className={styles.onlineVisit}>
+              <span>ویزیت آنلاین در پیام رسان:</span>
+              <div>
+                <MingcuteTelegramFill />
+                <MingcuteWhatsappFill />
+              </div>
+            </div>
+
+            <p>تضمین بازپرداخت مبلغ ویزیت در صورت نارضایتی</p>
+            <p>امکان برقراری تماس با پزشک وجود دارد</p>
+            <p>تا سه روز میتوانید هر سوالی دارید از پزشک بپرسید</p>
+            <button>شروع ویزیت با {doctor.name}</button>
+          </div>
         </div>
         <div className={styles.contact}>
-          <h2>نوبت‌دهی اینترنتی</h2>
+          <h4>نوبت‌دهی اینترنتی</h4>
           <p>برای دریافت نوبت اینترنتی، روی دکمه زیر کلیک کنید.</p>
           <button>دریافت نوبت</button>
         </div>
         <div className={styles.address}>
-          <h2>آدرس و شماره تلفن</h2>
-          <button>{doctor.address}</button>
-          <button>شماره تلفن: 021-12345678</button>
+          <h4>آدرس و شماره تلفن</h4>
+          <div className={styles.addressButton}>
+            <div>{doctor.address}</div>
+            <button>برنامه کاری پزشک</button>
+            <button>شماره تلفن: 021-12345678</button>
+            <button>مشاهده از روی نقشه و مسیریابی</button>
+          </div>
         </div>
       </div>
     </div>
   );
 }
-
 async function getDoctor(id: string): Promise<DoctorModel | undefined> {
   return new Promise((resolve) => {
     setTimeout((): void => {
